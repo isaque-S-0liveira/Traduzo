cba1537c1b6eed4e25344cfbfa58f67414b86c73
Isaque <zazac3179@gmail.com>
2023-09-15 01:12:49 -0300

    implementação de teste para o método list_as_json() da classe HistoryModel

--- tests/models/history/conftest.py
+++ tests/models/history/conftest.py
@@ -9,6 +9,7 @@ def prepare_base():
     db.get_collection("history").drop()
     HistoryModel(
         {
+            '_id': '6503c230af44b9952872f0ad',
             "text_to_translate": "Hello, I like videogame",
             "translate_from": "en",
             "translate_to": "pt",
@@ -17,6 +18,7 @@ def prepare_base():
 
     HistoryModel(
         {
+            '_id': '6503c230af44b9952872f0ae',
             "text_to_translate": "Do you love music?",
             "translate_from": "en",
             "translate_to": "pt",
--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,23 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    history_json = HistoryModel.list_as_json()
+    expected_data = [
+        {
+            "_id": "6503c230af44b9952872f0ad",
+            "text_to_translate": "Hello, I like videogame",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+        {
+            "_id": "6503c230af44b9952872f0ae",
+            "text_to_translate": "Do you love music?",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+    ]
+    history_data = json.loads(history_json)
+    assert history_data == expected_data

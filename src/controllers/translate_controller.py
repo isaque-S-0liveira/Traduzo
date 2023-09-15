73e0f937d8cf661359affdaacf58e367fb61b3e0
Isaque <zazac3179@gmail.com>
2023-09-14 21:55:40 -0300

    implementação da função index, que traduz o texto na rota post

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template, request
 from models.language_model import LanguageModel
-
-# from deep_translator import GoogleTranslator
+from deep_translator import GoogleTranslator
 
 # from models.history_model import HistoryModel
 
@@ -9,11 +8,16 @@ from models.language_model import LanguageModel
 translate_controller = Blueprint("translate_controller", __name__)
 
 
+def translate_text(tr_to, tr_from, text):
+    translated = GoogleTranslator(source=tr_from, target=tr_to).translate(text)
+    return translated
+
+
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
-    languages = LanguageModel.find()
-    default_text = "O que deseja traduzir"
+    languages = LanguageModel.list_dicts()
+    default_text = ""
     default_translate_from = "en"
     default_translate_to = "pt"
     translated = "Tradução"
@@ -24,6 +28,18 @@ def index():
             "translate-from", default_translate_from
         )
         translate_to = request.form.get("translate-to", default_translate_to)
+        translated = translate_text(
+            translate_to, translate_from, text_to_translate
+        )
+        return render_template(
+            "index.html",
+            languages=languages,
+            text_to_translate=text_to_translate,
+            translate_from=translate_from,
+            translate_to=translate_to,
+            translated=translated,
+        )
+
     else:
         text_to_translate = default_text
         translate_from = default_translate_from
